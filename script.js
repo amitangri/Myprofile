3385851a8c1c7d1aacc33b69e06c56add921d729
amitangri <amit.tangri@rediffmail.com>
2020-07-26 21:56:21 +0530

    2nd answer added

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+let closeButton = document.getElementById("closebut")
 let button1 = document.getElementById("but1")
 let ans1 = document.getElementById("first")
-let mainBlog = document.getElementById("mblog")
+let button2 = document.getElementById("but2")
+let ans2 = document.getElementById("second")
 
+let close =() =>{
+    ans1.style.display='none';
+    button1.innerHTML="Find Answer";
+    ans2.style.display='none';
+    button2.innerHTML="Find Answer"    
+}
+
+closeButton.onclick = close
 
 button1.onclick=()=>{
     if(ans1.style.display==='none'){
@@ -13,4 +23,13 @@ button1.onclick=()=>{
         button1.innerHTML="Find Answer"
 }
 }
-
+button2.onclick=()=>{
+    if(ans2.style.display==='none'){
+        ans2.style.display='inline';
+        button2.innerHTML="Squeeze Answer"
+}
+    else{
+        ans2.style.display='none';
+        button2.innerHTML="Find Answer"
+}
+}
